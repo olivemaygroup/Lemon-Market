30a623ecf72f6b49c30fa66e1332824d936e95d2
Deed G <vertigomatrix@protonmail.com>
2024-01-27 17:29:08 +0000

    build: router and controller

---
+++ server/controllers/review.controller.ts
@@ -0,0 +1,20 @@
+
+
+const addReview = async (ctx) => {
+
+};
+
+const myReviews = async (ctx) => {
+
+};
+
+const editReview = async (ctx) => {
+
+};
+
+const deleteReview = async (ctx) => {
+
+};
+
+
+export default { addReview, myReviews, editReview, deleteReview };
---
+++ server/controllers/userprofile.controller.ts
@@ -0,0 +1,28 @@
+
+
+const signup = async (ctx) => {
+
+};
+
+const addProfile = async (ctx) => {
+
+};
+
+const login = async (ctx) => {
+
+};
+
+const myProfile = async (ctx) => {
+
+};
+
+const editProfile = async (ctx) => {
+
+};
+
+const deleteAccount = async (ctx) => {
+
+};
+
+
+export default { signup, addProfile, login, myProfile, editProfile, deleteAccount };
