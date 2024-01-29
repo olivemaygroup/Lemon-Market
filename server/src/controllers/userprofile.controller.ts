bfa726f9a4fd87b3ba6969855cb7d3707ff991f6
Deed G <vertigomatrix@protonmail.com>
2024-01-29 15:10:03 +0000

    fix: route setup: signup

--- server/src/controllers/userprofile.controller.ts
+++ server/src/controllers/userprofile.controller.ts
@@ -1,10 +1,28 @@
 import prisma from "../db";
 import { Context } from "koa";
 
+interface Contact {
+  firstName: string, 
+  lastName: string,
+  email: string,
+  password: string
+}
+
 const signup = async (ctx: Context) => {
+  const { firstName, lastName, email, password } = <Contact> ctx.request.body
   try {
+    const newUser = await prisma.tenant.create({
+      data: {
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          password: password
+        }
+      })
     ctx.status = 200;
-  } catch (error) {}
+  } catch (error) {
+    console.log('error signing up user', error)
+  }
 };
 
 // const addProfile = async (ctx) => {
