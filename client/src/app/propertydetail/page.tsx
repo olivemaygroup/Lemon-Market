4d44490e4c11b15fb16998ff8751ca1600d3a140
Oliver Clyde <ollieclyde@hotmail.co.uk>
2024-02-06 16:23:18 +0000

    robot chat bot added

--- client/src/app/propertydetail/page.tsx
+++ client/src/app/propertydetail/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Image from "next/image";
 import { FaRobot } from "react-icons/fa";
+import Link from 'next/link'
+import Button from '@mui/material/Button'
 
 
 import styles from "@/app/propertydetail/page.module.css";
 import './page.module.css'
 import TopImageScroll from "@/app/components/PropertyDetail/propertyOverview";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/lib/store";
 import reviewAPI from "../ApiServices/reviewAPI";
@@ -16,16 +18,28 @@ import FullReview from "@/app/components/PropertyDetail/fullReview";
 import RatingDetail from "../components/PropertyDetail/ratingDetail";
 import { Review } from '@/app/types/review-types';
 import AddReview from "../components/PropertyDetail/addReview";
-import { Button, Classes, Popover } from "@blueprintjs/core";
+import { Popover } from "@blueprintjs/core";
 
 
 export default function PropertyDetail() {
   const dispatch = useDispatch();
 
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const handlePopoverInteraction = (nextOpenState: boolean) => {
+    setIsPopoverOpen(nextOpenState);
+  };
+
+  const chatBotPopUpText = 'Click me again to speak to an helpful AI assistant about any of your housing issues'
+
+  const chatBotHandler = () => {
+    // if (true){
+
+    // }
+  }
 
   const property = useSelector((state: RootState) => state.property.value)
   const reviewList: Review[] = useSelector((state: RootState) => state.reviewList.value)
-  console.log('review list state: ', reviewList)
 
   // useEffect(() => {
 
@@ -60,9 +74,23 @@ export default function PropertyDetail() {
           <FullReview item={item} />
         </div>
       ))}
-      <button className={styles.robot_button_container}>
-        <FaRobot className={styles.robot_icon} />
-      </button>
-    </div>
+      <Popover
+        onInteraction={handlePopoverInteraction}
+        isOpen={isPopoverOpen}
+        placement={'right-end'}
+        content={
+          <div className={styles.chat_bot_popup}>
+            <p>{chatBotPopUpText}</p>
+            <Link className={styles.link} href="/chatbot">
+              <Button className={styles.chat_bot_link} variant="contained">AI Chat Bot </Button>
+            </Link>
+          </div>
+        }
+      >
+        <button className={styles.robot_button_container}>
+          <FaRobot className={styles.robot_icon} />
+        </button>
+      </Popover>
+    </div >
   );
 }
