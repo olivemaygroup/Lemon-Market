c5fb5714093844be870ac81a593b2a79727d8cfd
Oliver Clyde <ollieclyde@hotmail.co.uk>
2024-02-07 18:59:42 +0000

    start of setup for deploying backend server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,15 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import cors from "@koa/cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = new Koa();
 
 import router from "./routers/router";
 
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3000;
 
 app
   .use(bodyParser())
@@ -14,6 +17,6 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(PORT, () => {
-  console.log(`Server connected on http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server connected on http://localhost:${PORT}`);
+  });
